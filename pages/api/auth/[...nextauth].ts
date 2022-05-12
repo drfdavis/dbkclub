4b23e99c8b87a7da2ac31054790379dda29200d7
Quavo Huncho <wartywarthog7@gmail.com>
2022-05-12 10:18:58 +0000

    another edit

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,9 +2,7 @@
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import { PrismaClient } from '@prisma/client'
 import NextAuth from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github'
 import GoogleProvider from 'next-auth/providers/google'
-import EmailProvider from 'next-auth/providers/email'
 
 const prisma = new PrismaClient()
 
@@ -15,24 +13,5 @@ export default NextAuth({
 			clientId: process.env.GOOGLE_CLIENT_ID as string,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
 		}),
-		// GitHubProvider({
-		// 	clientId: process.env.GITHUB_ID as string,
-		// 	clientSecret: process.env.GITHUB_SECRET as string,
-		// }),
-		// EmailProvider({
-		// 	server: process.env.EMAIL_SERVER,
-		// 	from: process.env.EMAIL_FROM,
-		// }),
 	],
-	secret: process.env.NEXTAUTH_SECRET as string,
-	callbacks: {
-		// session: async ({ session, user }) => {
-		// 	// Over here, I had to add the `user` property to the session object
-		// 	// I added a new field to the session object in environment.d.ts (implemented later)
-		// 	if (session?.user) {
-		// 		session.user.id = user.id
-		// 	}
-		// 	return session
-		// },
-	},
 })
