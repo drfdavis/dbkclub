a8e11d0b7820190e9d7381aaa3214109168e3d9d
Quavo Huncho <wartywarthog7@gmail.com>
2022-05-24 21:25:17 +0000

    fix: :sparkles: update index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,123 +1,112 @@
-import React from 'react'
-import type { NextPage } from 'next'
 import {
-	chakra,
 	Box,
-	useColorModeValue,
 	Button,
+	Flex,
+	Heading,
+	Image,
 	Stack,
 	Text,
+	useBreakpointValue,
 } from '@chakra-ui/react'
-import Link from 'next/link'
-import { signIn, useSession } from 'next-auth/react'
-import { FcGoogle } from 'react-icons/fc'
 
-const IndexHero: NextPage = () => {
-	const { data: session, status } = useSession();
-	console.log(session?.user?.user!.email);
+import { signIn, useSession } from 'next-auth/react'
+import Link from 'next/link'
+
+export default function SplitScreen() {
+	const { data: session, status } = useSession()
 	return (
-		<div className='svg-background'>
-			<Box px={8} py={24} mx='auto'>
+		<Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+			<Flex
+				p={8}
+				flex={1}
+				align={'center'}
+				justify={'center'}
+				position='relative'
+			>
+				{/* <Text position='absolute' top={10} left={10} display='block' mb={2}>
+					yoo
+				</Text> */}
 				<Box
-					w={{ base: 'full', md: 11 / 12, xl: 9 / 12 }}
-					mx='auto'
-					textAlign={{ base: 'left', md: 'center' }}
-					display='grid'
-					placeContent='center'
-					minH='60vh'
+					display='flex'
+					alignContent='center'
+					alignItems='center'
+					position='absolute'
+					top={10}
+					left={10}
+					mb={2}
 				>
-					<chakra.h1
-						mb={6}
-						fontSize={{ base: '4xl', md: '7xl' }}
-						fontWeight='semibold'
-						lineHeight='none'
-						letterSpacing={{ base: 'normal', md: 'tight' }}
-						color={useColorModeValue('gray.700', 'gray.100')}
+					{/* <Image src='/dbk.png' width={10} alt='Hero' /> */}
+					{/* <Text display='inline-block' textTransform='uppercase' fontWeight='semibold'>Diamond Back</Text> */}
+				</Box>
+				<Stack spacing={6} w={'full'} maxW={'lg'}>
+					{/* <Text mt={'-200'}>yo</Text> */}
+					<Heading
+						fontSize={{ base: '3xl', md: '4xl', lg: '5xl' }}
+						mt={{ base: 20, md: 0 }}
 					>
-						Easy and safe{' '}
 						<Text
-							display={{ base: 'block', lg: 'inline' }}
-							w='full'
-							bgClip='text'
-							bgGradient='linear(to-r, green.400,purple.500)'
-							fontWeight='semibold'
+							as={'span'}
+							position={'relative'}
+							_after={{
+								content: "''",
+								width: 'full',
+								height: useBreakpointValue({ base: '20%', md: '30%' }),
+								position: 'absolute',
+								bottom: 1,
+								left: 0,
+								bg: 'blue.400',
+								zIndex: -1,
+							}}
 						>
-							tokenized
+							Fast
+						</Text>
+						<br />{' '}
+						<Text color={'blue.400'} as={'span'}>
+							Tokenized Transactions
 						</Text>{' '}
-						transactions, done well.
-					</chakra.h1>
-					<chakra.p
-						px={{ base: 0, lg: 24 }}
-						mb={6}
-						fontSize={{ base: 'lg', md: 'xl' }}
-						color={useColorModeValue('gray.600', 'gray.300')}
-					>
-						Easily send and receive money across borders, and more. Use loyalty,
-						royalty or bonus tokens for rewards. Create and manage your own
-						tokens.
-					</chakra.p>
-					<Stack
-						direction={{ base: 'column', sm: 'row' }}
-						mb={{ base: 4, md: 8 }}
-						spacing={2}
-						justifyContent={{ sm: 'left', md: 'center' }}
-					>
-						{!session?.user ? (
-							<>
+					</Heading>
+					<Text fontSize={{ base: 'md', lg: 'lg' }} color={'gray.500'}>
+						Sign in to enjoy the benefits of the Diamond Back Token Systems
+					</Text>
+					<Stack direction={{ base: 'column', md: 'row' }} spacing={4}>
+						{session?.user.user ? (
+							<Link href='/app'>
 								<Button
-									onClick={() => signIn()}
-									// leftIcon={<FcGoogle />}
-									isLoading={status === 'loading'}
-									disabled={status === 'loading'}
-									size='sm'
-									px={5}
-									py={2}
-									bg='blue.400'
-									color='white'
-									variant='outline'
+									rounded={'full'}
+									bg={'blue.400'}
+									color={'white'}
 									_hover={{
 										bg: 'blue.500',
 									}}
-									_active={{
-										bg: 'blue.600',
-									}}
+									width={{ base: 'full', sm: '50%', md: '30%' }}
 								>
-									Sign In
+									Dashboard
 								</Button>
-							</>
+							</Link>
 						) : (
-							<>
-								<Link href='/app' passHref>
-									<Button
-										size='sm'
-										bg='blue.500'
-										color='white'
-										_hover={{
-											bg: 'blue.600',
-										}}
-										_active={{
-											bg: 'blue.700',
-										}}
-										px={5}
-									>
-										Dashboard
-									</Button>
-								</Link>
-							</>
+							<Button
+								rounded={'full'}
+								bg={'blue.400'}
+								color={'white'}
+								_hover={{
+									bg: 'blue.500',
+								}}
+								width={{ base: 'full', sm: '50%', md: '30%' }}
+								onClick={() => signIn()}
+							>
+								Log In
+							</Button>
 						)}
 					</Stack>
-				</Box>
-			</Box>
-		</div>
+				</Stack>
+			</Flex>
+			<Flex flex={1}>
+				<Image
+					alt={'Login Image'}
+					objectFit={'cover'}
+					src={'/animated-wave.svg'}
+				/>
+			</Flex>
+		</Stack>
 	)
 }
-
-export default IndexHero
-
-
-
-
-
-
-
-
